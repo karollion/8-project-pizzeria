a6e7a0a4319f073aa5dced07091cd38122478315
Karol Bernatowicz <karol.bernatowicz@gmail.com>
2023-05-26 19:32:54 +0200

    Add acordeon to menu

--- src/js/script.js
+++ src/js/script.js
@@ -58,7 +58,8 @@ const select = {
       thisProduct.id = id;
       thisProduct.data = data;
       thisProduct.renderInMenu();
-      console.log('new Product', thisProduct);
+      thisProduct.initAccordion();
+      //console.log('new Product', thisProduct);
     }
 
     renderInMenu() {
@@ -76,12 +77,34 @@ const select = {
       menuContainer.appendChild(thisProduct.element);
     }
 
+    initAccordion(){
+      const thisProduct = this;
+  
+      /* find the clickable trigger (the element that should react to clicking) */
+      const clickableTrigger = thisProduct.element.querySelector(select.menuProduct.clickable);
+  
+      /* START: add event listener to clickable trigger on event click */
+      clickableTrigger.addEventListener('click', function(event) {
+        /* prevent default action for event */
+        event.preventDefault();
+        /* find active product (product that has active class) */
+        const activeProduct = document.querySelector(select.all.menuProductsActive);
+        console.log('activeProduct: ', activeProduct);
+        /* if there is active product and it's not thisProduct.element, remove class active from it */
+        if(activeProduct !== null && activeProduct !== thisProduct.element) {
+          activeProduct.classList.remove(classNames.menuProduct.wrapperActive);
+        }
+        /* toggle active class on thisProduct.element */
+        thisProduct.element.classList.toggle(classNames.menuProduct.wrapperActive); //activeProduct.classNames.toogle //.classNames.toogle('active');
+      });
+  
+    }
   }
 
   const app = {
     initMenu: function(){
       const thisApp = this;
-      console.log('thisApp.data', thisApp.data);
+      //console.log('thisApp.data', thisApp.data);
       for(let productData in thisApp.data.products){
         new Product(productData, thisApp.data.products[productData]);
       }

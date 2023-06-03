a813f181186962fcfac66f042837894809e4de6c
Karol Bernatowicz <karol.bernatowicz@gmail.com>
2023-06-03 22:22:04 +0200

    Add function to remove product from cart

--- src/js/script.js
+++ src/js/script.js
@@ -378,6 +378,10 @@
       thisCart.dom.productList.addEventListener('updated', function(){
         thisCart.update();
       });
+
+      thisCart.dom.productList.addEventListener('remove', function(){
+        thisCart.remove(event.detail.cartProduct);
+      });
     }
 
     add(menuProduct){
@@ -419,6 +423,17 @@
       }
       console.log(totalNumber, subtotalPrice, thisCart.totalPrice);
     }
+
+    remove(product){
+      const thisCart = this;
+
+      /* Remove product from DOM */
+      product.dom.wrapper.remove();
+      /* Remove product from product list in cart*/
+      thisCart.products.splice(thisCart.products.indexOf(product), 1);
+      /* Update cart*/
+      thisCart.update();
+    }
   }
 
   class CartProduct{
@@ -434,6 +449,7 @@
       
       thisCartProduct.getElements(element);
       thisCartProduct.initAmountWidget();
+      thisCartProduct.initActions();
       //console.log(thisCartProduct);
     }
 
@@ -455,6 +471,32 @@
         thisCartProduct.amount = thisCartProduct.amountWidget.value;
         thisCartProduct.price = thisCartProduct.priceSingle * thisCartProduct.amount;
         thisCartProduct.dom.price.innerHTML = thisCartProduct.price;
+      });
+    }
+
+    remove(){
+      const thisCartProduct = this;
+
+      const event = new CustomEvent('remove', {
+        bubbles: true,
+        detail: {
+          cartProduct: thisCartProduct,
+        },
+      });
+
+      thisCartProduct.dom.wrapper.dispatchEvent(event);
+    }
+
+    initActions(){
+      const thisCartProduct = this;
+
+      thisCartProduct.dom.edit.addEventListener('click', function(){
+        event.preventDefault();
+        
+      });
+      thisCartProduct.dom.remove.addEventListener('click', function(){
+        event.preventDefault();
+        thisCartProduct.remove();
       });
     }
   }

a27288bf5f8a5019574aa7e9b29dd032e3494401
Karol Bernatowicz <karol.bernatowicz@gmail.com>
2023-06-10 16:57:52 +0200

    Add new styles and elements to to handle the reservation

--- dist/js/components/Product.js
+++ dist/js/components/Product.js
@@ -141,7 +141,7 @@ class Product {
     const event = new CustomEvent('add-to-cart', {
       bubbles: true,
       detail: {
-          product: thisProduct.prepareCartProduct(),
+        product: thisProduct.prepareCartProduct(),
       },
     });
     thisProduct.element.dispatchEvent(event);
